f3e76f95fd28dc058b244c93207b3aedb872591c
TukanDev <contact@tukandev.com>
2025-04-07 16:27:22 +0200

    Now processes launched have their log files and fix space support in folder names
    Signed-off-by: TukanDev <contact@tukandev.com>

--- src-tauri/src/utils/game_launch_manager.rs
+++ src-tauri/src/utils/game_launch_manager.rs
@@ -1,4 +1,9 @@
-use std::process::{Command, Stdio};
+use std::fs;
+use std::io::{Read, Write};
+use std::path::{Path, PathBuf};
+use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::process::{Child, Command, Stdio};
 use tauri::{AppHandle, Error};
 use crate::commands::settings::GlobalSettings;
 use crate::utils::repo_manager::{GameManifest, LauncherInstall};
@@ -28,7 +33,7 @@ pub fn launch(app: &AppHandle, install: LauncherInstall, gm: GameManifest, gs: G
     };
 
     if !pre_launch.is_empty() {
-        let command = format!("{runner}/{wine64} {pre_launch}");
+        let command = format!("'{runner}/{wine64}' '{pre_launch}'");
 
         let mut cmd = Command::new("bash");
         cmd.arg("-c");
@@ -44,7 +49,8 @@ pub fn launch(app: &AppHandle, install: LauncherInstall, gm: GameManifest, gs: G
 
         let spawned = cmd.spawn();
         if spawned.is_ok() {
-            spawned?;
+            let process = spawned?;
+            write_game_log(Path::new(&dir.clone()).to_path_buf(), process, "game.log".parse().unwrap());
         }
     }
 
@@ -54,11 +60,11 @@ pub fn launch(app: &AppHandle, install: LauncherInstall, gm: GameManifest, gs: G
             args = &install.launch_args;
         }
 
-        let mut command = format!("{runner}/{wine64} {dir}/{game} {args}");
+        let mut command = format!("'{runner}/{wine64}' '{dir}/{game}' {args}");
 
         if install.use_jadeite {
             let jadeite_path = gs.jadeite_path.clone();
-            command = format!("{runner}/{wine64} z:\\{jadeite_path}/jadeite.exe z:\\{dir}/{game} -- {args}");
+            command = format!("'{runner}/{wine64}' '{jadeite_path}/jadeite.exe' '{dir}/{game}' -- {args}");
         }
 
         let mut cmd = Command::new("bash");
@@ -88,9 +94,10 @@ pub fn launch(app: &AppHandle, install: LauncherInstall, gm: GameManifest, gs: G
 
         let spawned = cmd.spawn();
         if spawned.is_ok() {
-            spawned?;
+            let process = spawned?;
             load_xxmi(install.clone(), prefix.clone(), gs.xxmi_path, runner.clone(), wine64.clone(), exe.clone());
             load_fps_unlock(install, prefix, gs.fps_unlock_path, runner, wine64);
+            write_game_log(Path::new(&dir.clone()).to_path_buf(), process, "game.log".parse().unwrap());
             true
         } else {
             false
@@ -99,11 +106,11 @@ pub fn launch(app: &AppHandle, install: LauncherInstall, gm: GameManifest, gs: G
         // We assume user knows what he/she is doing so we just execute command that is configured without any checks
         let c = install.launch_command.clone();
         let args;
-        let mut command = format!("{runner}/{wine64} {c}");
+        let mut command = format!("'{runner}/{wine64}' '{c}'");
 
         if !install.launch_args.is_empty() {
             args = &install.launch_args;
-            command = format!("{runner}/{wine64} {c} {args}");
+            command = format!("'{runner}/{wine64}' '{c}' {args}");
         }
 
         let mut cmd = Command::new("bash");
@@ -116,7 +123,7 @@ pub fn launch(app: &AppHandle, install: LauncherInstall, gm: GameManifest, gs: G
         cmd.stdout(Stdio::piped());
         cmd.stderr(Stdio::piped());
         cmd.current_dir(dir.clone());
-        cmd.process_group(0); // Start as detached process so killing launcher does not kill the game
+        cmd.process_group(0);
 
         if !install.env_vars.is_empty() {
             let envs = install.env_vars.clone();
@@ -133,9 +140,10 @@ pub fn launch(app: &AppHandle, install: LauncherInstall, gm: GameManifest, gs: G
 
         let spawned = cmd.spawn();
         if spawned.is_ok() {
-            spawned?;
+            let process = spawned?;
             load_xxmi(install.clone(), prefix.clone(), gs.xxmi_path, runner.clone(), wine64.clone(), exe.clone());
             load_fps_unlock(install, prefix, gs.fps_unlock_path, runner, wine64);
+            write_game_log(Path::new(&dir.clone()).to_path_buf(), process, "game.log".parse().unwrap());
             true
         } else {
             false
@@ -151,7 +159,7 @@ fn load_xxmi(install: LauncherInstall, prefix: String, xxmi_path: String, runner
         std::thread::sleep(std::time::Duration::from_secs(3));
         println!("injecting xxmi loader...");
         let xxmi_path = xxmi_path.clone();
-        let command = format!("{runner}/{wine64} z:\\{xxmi_path}/injector.exe -n {game} z:\\{xxmi_path}/3dmloader.dll");
+        let command = format!("'{runner}/{wine64}' 'z:\\{xxmi_path}/injector.exe' -n {game} 'z:\\{xxmi_path}/3dmloader.dll'");
 
         let mut cmd = Command::new("bash");
         cmd.arg("-c");
@@ -160,11 +168,16 @@ fn load_xxmi(install: LauncherInstall, prefix: String, xxmi_path: String, runner
         cmd.env("WINEARCH","win64");
         cmd.env("WINEPREFIX", prefix.clone());
 
-        //cmd.stdout(Stdio::piped());
-        //cmd.stderr(Stdio::piped());
+        cmd.stdout(Stdio::piped());
+        cmd.stderr(Stdio::piped());
         cmd.current_dir(xxmi_path.clone());
         cmd.process_group(0);
-        cmd.spawn().unwrap();
+
+        let spawn = cmd.spawn();
+        if spawn.is_ok() {
+            let process = spawn.unwrap();
+            write_game_log(Path::new(&xxmi_path.clone()).to_path_buf(), process, "xxmi.log".parse().unwrap());
+        }
     }
 }
 
@@ -175,7 +188,7 @@ fn load_fps_unlock(install: LauncherInstall, prefix: String, fpsunlock_path: Str
     if install.use_fps_unlock {
         let fpsunlock_path = fpsunlock_path.clone();
         let fpsv = install.fps_value;
-        let command = format!("{runner}/{wine64} z:\\{fpsunlock_path}/fpsunlock.exe {fpsv} 3000");
+        let command = format!("'{runner}/{wine64}' 'z:\\{fpsunlock_path}/fpsunlock.exe' {fpsv} 3000");
 
         let mut cmd = Command::new("bash");
         cmd.arg("-c");
@@ -188,8 +201,111 @@ fn load_fps_unlock(install: LauncherInstall, prefix: String, fpsunlock_path: Str
         cmd.stderr(Stdio::piped());
         cmd.current_dir(fpsunlock_path.clone());
         cmd.process_group(0);
-        cmd.spawn().unwrap();
+
+        let spawn = cmd.spawn();
+        if spawn.is_ok() {
+            let process = spawn.unwrap();
+            write_game_log(Path::new(&fpsunlock_path.clone()).to_path_buf(), process, "fps_unlocker.log".parse().unwrap());
+        }
     }
+}
+
+#[cfg(target_os = "linux")]
+fn write_game_log(log_dir: PathBuf, child: Child, file: String) {
+    let ld1 = Arc::new(Mutex::new(log_dir.clone()));
+    let c1 = Arc::new(Mutex::new(child));
+    std::thread::spawn(move || {
+        let log_dir = ld1.lock().unwrap().clone();
+        let mut child = c1.lock().unwrap();
+
+        // Credit to certain anime team for this too lol pointless to write from scratch...
+        let game_output = Arc::new(Mutex::new(fs::File::create(log_dir.join(file)).unwrap()));
+        let written = Arc::new(AtomicUsize::new(0));
+
+        let mut stdout_join = None;
+        let mut stderr_join = None;
+
+        if let Some(mut stdout) = child.stdout.take() {
+            let game_output = game_output.clone();
+            let written = written.clone();
+
+            stdout_join = Some(std::thread::spawn(move || -> std::io::Result<()> {
+                let mut buf = [0; 1024];
+
+                while let Ok(read) = stdout.read(&mut buf) {
+                    if read == 0 {
+                        break;
+                    }
+
+                    let Ok(mut game_output) = game_output.lock() else {
+                        break;
+                    };
+
+                    for line in buf[..read].split(|c| c == &b'\n') {
+                        game_output.write_all(b"    [stdout] ")?;
+                        game_output.write_all(line)?;
+                        game_output.write_all(b"\n")?;
+
+                        written.fetch_add(line.len() + 14, Ordering::Relaxed);
+                    }
+
+                    if written.load(Ordering::Relaxed) > 10000 {
+                        break;
+                    }
+                }
+
+                Ok(())
+            }));
+        }
+
+        if let Some(mut stderr) = child.stderr.take() {
+            let game_output = game_output.clone();
+            let written = written.clone();
+
+            stderr_join = Some(std::thread::spawn(move || -> std::io::Result<()> {
+                let mut buf = [0; 1024];
+
+                while let Ok(read) = stderr.read(&mut buf) {
+                    if read == 0 {
+                        break;
+                    }
+
+                    let Ok(mut game_output) = game_output.lock() else {
+                        break;
+                    };
+
+                    for line in buf[..read].split(|c| c == &b'\n') {
+                        game_output.write_all(b"[!] [stderr] ")?;
+                        game_output.write_all(line)?;
+                        game_output.write_all(b"\n")?;
+
+                        written.fetch_add(line.len() + 14, Ordering::Relaxed);
+                    }
+
+                    if written.load(Ordering::Relaxed) > 10000 {
+                        break;
+                    }
+                }
+
+                Ok(())
+            }));
+        }
+
+        child.wait().unwrap();
+        if let Ok(mut file) = game_output.lock() {
+            file.flush().unwrap();
+        }
+
+        drop(game_output);
+
+        if let Some(join) = stdout_join {
+            join.join().map_err(|err| format!("Failed to join stdout reader thread: {err:?}")).unwrap().unwrap();
+        }
+
+        if let Some(join) = stderr_join {
+            join.join().map_err(|err| format!("Failed to join stderr reader thread: {err:?}")).unwrap().unwrap();
+        }
+    });
 }
 
 #[cfg(target_os = "windows")]
@@ -204,5 +320,10 @@ fn load_xxmi(_install: LauncherInstall, _prefix: String, _xxmi_path: String, _ru
 
 #[cfg(target_os = "windows")]
 fn load_fps_unlock(_install: LauncherInstall, _prefix: String, _fpsunlock_path: String, _runner: String, _wine64: String) {
+
+}
+
+#[cfg(target_os = "windows")]
+fn write_game_log(_log_dir: PathBuf, _child: Child) {
 
 }
