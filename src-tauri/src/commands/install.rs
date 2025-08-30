d3189cb96552ccccb79689dd3e19e1be4a3b3b3f
TukanDev <contact@tukandev.com>
2025-08-30 02:43:03 +0200

    Auto ignore wuwa updates if we add steam install
    Signed-off-by: TukanDev <contact@tukandev.com>

--- src-tauri/src/commands/install.rs
+++ src-tauri/src/commands/install.rs
@@ -61,7 +61,7 @@ pub fn get_install_by_id(app: AppHandle, id: String) -> Option<String> {
 
 #[allow(unused_mut, unused_variables)]
 #[tauri::command]
-pub fn add_install(app: AppHandle, manifest_id: String, version: String, audio_lang: String, name: String, mut directory: String, mut runner_path: String, mut dxvk_path: String, runner_version: String, dxvk_version: String, game_icon: String, game_background: String, ignore_updates: bool, skip_hash_check: bool, mut use_jadeite: bool, use_xxmi: bool, use_fps_unlock: bool, env_vars: String, pre_launch_command: String, launch_command: String, fps_value: String, mut runner_prefix: String, launch_args: String, skip_game_dl: bool) -> Option<AddInstallRsp> {
+pub fn add_install(app: AppHandle, manifest_id: String, version: String, audio_lang: String, name: String, mut directory: String, mut runner_path: String, mut dxvk_path: String, runner_version: String, dxvk_version: String, game_icon: String, game_background: String, mut ignore_updates: bool, skip_hash_check: bool, mut use_jadeite: bool, use_xxmi: bool, use_fps_unlock: bool, env_vars: String, pre_launch_command: String, launch_command: String, fps_value: String, mut runner_prefix: String, launch_args: String, skip_game_dl: bool) -> Option<AddInstallRsp> {
     if manifest_id.is_empty() || version.is_empty() || name.is_empty() || directory.is_empty() || runner_path.is_empty() || dxvk_path.is_empty() || game_icon.is_empty() || game_background.is_empty() {
         None
     } else {
@@ -75,6 +75,12 @@ pub fn add_install(app: AppHandle, manifest_id: String, version: String, audio_l
         let install_location = if skip_game_dl { Path::new(directory.as_str()).follow_symlink().unwrap().to_path_buf() } else { Path::new(directory.as_str()).join(cuid.clone()).follow_symlink().unwrap().to_path_buf() };
         if !install_location.exists() { fs::create_dir_all(&install_location).unwrap(); }
         directory = install_location.to_str().unwrap().to_string();
+
+        // If wuwa is steam build auto ignore updates
+        if gm.biz == "wuwa_global" && skip_game_dl {
+            let steamdll = install_location.join("Client/Binaries/Win64/steam_api64.dll");
+            if steamdll.exists() { ignore_updates = true; }
+        }
 
         #[cfg(target_os = "windows")]
         {
