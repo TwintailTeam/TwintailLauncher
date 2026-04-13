f5880f0c00ba2b3d3e7793c2cc08002a8a98a967
TukanDev <contact@tukandev.com>
2026-04-13 17:31:26 +0200

    Some cleanups and boring maintenance
    Signed-off-by: TukanDev <contact@tukandev.com>

--- src/components/popups/POPUPS.ts
+++ src/components/popups/POPUPS.ts
@@ -3,7 +3,5 @@ export enum POPUPS {
     REPOMANAGER = 'popups/repomanager',
     ADDREPO = 'popups/addrepo',
     DOWNLOADGAME = 'popups/downloadgame',
-    DOWNLOADS = 'popups/downloads',
     INSTALLSETTINGS = 'popups/installationsettings',
-    RUNNERMANAGER = 'popups/runnermanager'
 }
